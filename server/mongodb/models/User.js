8865d40748cd054febd28e9d0344cb03db606f27
Daniel Yoo <dacapo0509@gmail.com>
2023-05-29 21:17:28 -0700

    Add User signup backend endpoint

--- server/mongodb/models/User.js
+++ server/mongodb/models/User.js
@@ -3,30 +3,16 @@ const bcrypt = require("bcryptjs");
 
 const User = new mongoose.Schema({
 	name: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-});
-
-// Hash the password before saving or updating the user
-User.pre("save", function (next) {
-	if (!this.isModified("password")) {
-		return next();
-	}
-
-	bcrypt.genSalt(10, (err, salt) => {
-		if (err) {
-			return next(err);
-		}
-
-		bcrypt.hash(this.password, salt, (err, hash) => {
-			if (err) {
-				return next(err);
-			}
-
-			this.password = hash;
-			next();
-		});
-	});
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+	},
 });
 
 const UserModel = mongoose.model("User", User);
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,4 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const UserModel = require("../../mongodb/models/User");
+// const connectDB = require("../../mongodb/connect.js");
+
+// Signup
+router.post("/", async (req, res) => {
+	const { email, password, name } = req.body;
+	const hashedPassword = bcrypt.hashSync(password);
+	try {
+		// Check if the email already exists
+		const existingUser = await UserModel.findOne({ email });
+		if (existingUser) {
+			// Email address already exists
+			return res
+				.status(400)
+				.json({ error: "Email address already exists" });
+		}
+		//Check if user included name or password
+		if (!name || !password) {
+			return res
+				.status(400)
+				.json({ error: "Please input your name/password" });
+		}
+		//Check password length
+		if (password.length < 6) {
+			return res.status(400).json({
+				error: "Password length must be at least 6 characters",
+			});
+		}
+		const newUser = new UserModel({
+			name,
+			email,
+			password: hashedPassword,
+		});
+		newUser.save();
+		const safeUser = {
+			id: newUser._id,
+			email: newUser.email,
+			name: newUser.name,
+		};
+		await setTokenCookie(res, safeUser);
+		return res.json({
+			user: safeUser,
+		});
+	} catch (error) {
+		// Handle other error cases, if any
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
+});
 
 module.exports = router;
