c08edccf82ab67e1d38f3c319804dd71ecd2689f
Daniel Yoo <dacapo0509@gmail.com>
2023-06-02 12:19:38 -0700

    started css on page. Added Navbar css and homepage. Working on login next

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,53 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import "../css/Navbar.css";
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
 
 	return (
-		<div>
-			<div>
-				<Link exact to="/">
+		<div className="navBody">
+			<nav className="navMenu">
+				<Link className="homeLink" exact to="/">
 					Home
 				</Link>
-			</div>
-			<div>
+
 				{sessionUser ? (
 					<div>{<ProfileButton user={sessionUser} />}</div>
 				) : (
 					<div>
-						<Link to="/login">Log In</Link>
-						<Link to="/signup">Sign Up</Link>
+						<Link className="loginLink" to="/login">
+							Log In
+						</Link>
+						<Link className="signupLink" to="/signup">
+							Sign Up
+						</Link>
 					</div>
 				)}
-			</div>
+				<div className="dot"></div>
+			</nav>
 		</div>
 	);
+	// return (
+	// 	<div>
+	// 		<div>
+	// 			<Link exact to="/">
+	// 				Home
+	// 			</Link>
+	// 		</div>
+	// 		<div>
+	// 			{sessionUser ? (
+	// 				<div>{<ProfileButton user={sessionUser} />}</div>
+	// 			) : (
+	// 				<div>
+	// 					<Link to="/login">Log In</Link>
+	// 					<Link to="/signup">Sign Up</Link>
+	// 				</div>
+	// 			)}
+	// 		</div>
+	// 	</div>
+	// );
 }
 
 export default Navigation;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
+import "../css/Homepage.css";
+
 const HomePage = () => {
-	return <div>This is the home page! Much wow.</div>;
+	return (
+		<div className="homeWrapper">
+			<h1 className="homeHeader">THIS IS THE HOME PAGE. MUCH WOW</h1>
+		</div>
+	);
 };
 
 export default HomePage;
